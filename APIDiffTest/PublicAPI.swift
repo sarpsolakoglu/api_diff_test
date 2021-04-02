8aa217f5cda1f23ca61fb943aea408da7cfa8cdb
Sarp Solakoglu <ssolakoglu@zendesk.com>
2021-04-02 10:19:10 +0100

    Add public API

---
+++ APIDiffTest/PublicAPI.swift
@@ -0,0 +1,16 @@
+//
+//  PublicAPI.swift
+//  APIDiffTest
+//
+//  Created by Sarp Solakoglu on 02/04/2021.
+//
+
+import Foundation
+
+public class PublicAPI {
+    public var publicVar: String?
+
+    public func publicFunc() -> String {
+        return ""
+    }
+}
