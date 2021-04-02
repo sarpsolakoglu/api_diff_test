b5b2314670a2ea646787f7e265d2fecb60c005d8
Sarp Solakoglu <ssolakoglu@zendesk.com>
2021-04-02 10:06:53 +0100

    Initial Commit

---
+++ APIDiffTest/APIDiffTest.h
@@ -0,0 +1,18 @@
+//
+//  APIDiffTest.h
+//  APIDiffTest
+//
+//  Created by Sarp Solakoglu on 02/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIDiffTest.
+FOUNDATION_EXPORT double APIDiffTestVersionNumber;
+
+//! Project version string for APIDiffTest.
+FOUNDATION_EXPORT const unsigned char APIDiffTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIDiffTest/PublicHeader.h>
+
+
